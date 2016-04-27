e20c9eb60eeda2278e3fdebbd373cf59ddf31174
MariaRomero <fantasysystems@gmail.com>
2016-04-27 15:42:09 +0100

    challenge 21

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,18 +1,12 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Dave'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Dave vs. Mittens'
   end
 end
 feature 'Hit points' do
   scenario 'view other players hit points' do
-    visit('/')
-        fill_in :player_1_name, with: 'Dave'
-        fill_in :player_2_name, with: 'Mittens'
-        click_button 'Submit'
-        expect(page).to have_content ('Mittens: 60HP')
-    end
+    sign_in_and_play
+    expect(page).to have_content ('Mittens: 60HP')
   end
+end
