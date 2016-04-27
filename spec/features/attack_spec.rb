e83971f7e9f6040eb1d05737a44d2a93f892c60b
Robin Heathcote <robin.heathcote@icloud.com>
2016-04-27 18:17:32 +0100

    Challege 24: Still not working

---
+++ spec/features/attack_spec.rb
@@ -0,0 +1,14 @@
+feature 'Attack:' do
+  scenario 'attack player and receive confirmation' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content ('Dave attacked Mittens')
+  end
+
+  scenario 'attack reduces opponents HP' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content ('Mittens: 60HP')
+    expect(page).to have_content ('Mittens: 50HP')
+  end
+end
