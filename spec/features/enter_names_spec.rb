acc637fdd8b6578f5e20d8dd986bdb87809b85cf
Robin Heathcote <robin.heathcote@icloud.com>
2016-04-27 16:15:40 +0100

    Challenge 22: Allow Maria to attack me

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -10,3 +10,11 @@ feature 'Hit points' do
     expect(page).to have_content ('Mittens: 60HP')
   end
 end
+
+feature 'Attack' do
+  scenario 'attack player and receive confirmation' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).to have_content ('Dave attacked Mittens')
+  end
+end
